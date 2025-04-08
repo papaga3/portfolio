4393d139af3ecb287a00bc20289247b3bff21200
Huy Nguyen <ng.q.huy98@gmail.com>
2025-04-08 18:24:00 +0300

    small update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -19,15 +19,15 @@ const Home = () => {
       <div className="grid grid-cols-1 md:grid-cols-2">
         <div className="m-4 text-justify">
           <p> My name is Quoc Huy, Nguyen. I am a software developer from Vaasa, Finland. I graduated from VAMK in 2021. 
-              I have experiences working as a web developer using ReactJS. I am familiar with JavaSpring and .NET framework.
+              I have experiences working as a web developer using ReactJS. I am also familiar with JavaSpring and .NET framework.
           </p>
-          <Button>
-            <Link href="/about"> About me </Link>
-          </Button>
         </div>
 
-        <div className="border m-4">
-            <h2> Here is a list of my skills </h2>
+        <div className="m-4">
+            <h3 className="text-xl font-bold"> Here is a list of my skills </h3>
+            { skills.map((item) => 
+                <div className="inline-block m-2 bg-pink-300 border rounded-sm text-center text-neutral-900 w-80 md:w-24"> {item} </div>) 
+            }
         </div>
       </div>
 
